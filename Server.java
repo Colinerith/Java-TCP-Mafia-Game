c111bb01bea109c65d16513a085486c9f0c8289e
HJ <HJ@HJ-INHA>
2020-12-17 23:18:54 +0900

    day6 Server 함수 분리 & design4

--- Server.java
+++ Server.java
@@ -10,43 +10,60 @@ import java.util.Scanner;
 
 public class Server implements Runnable {
 	private static Socket clientSocket;
+	static Scanner scv = new Scanner(System.in);
 	
 	public Server(Socket clientSocket) {
 		this.clientSocket = clientSocket;
-	} 
-	public static void main(String[] args) {
-		Scanner scv = new Scanner(System.in);
-		
-		// 이 아래부터 함수로 따로 빼야할 것 같
-		System.out.println("Game start...");
+	}
+	
+	public static void waiting_room() { // 대기실. 플레이어들이 모두 모여야 시작
+		System.out.println("NEW GAME");
 		System.out.print("Enter the number of players(5~8): ");
 		int playerNum = scv.nextInt();
-		//int temp = playerNum;
 		try(ServerSocket sSocket = new ServerSocket(10000)){
-			//계속 접속하는 클라이언트들에 대한 무한 서비스를 하려면
-			//while(true) {
 			for(int j=0; j < playerNum; j++){ //입력한 플레이어 수만큼 클라이언트 대기
-				System.out.println("연결 대기 중...");
+				//System.out.println("연결 대기 중...");
 				clientSocket = sSocket.accept();
-				Server tes = new Server(clientSocket);//개별스레드 생성. 수락 했을 때의 client 주소(정보)가 담김
+				Server tes = new Server(clientSocket); //개별스레드 생성. 수락 했을 때의 client 주소(정보)가 담김
 				new Thread(tes).start();
 			}
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-		System.out.println("The Winner is: ");
-		System.out.println("Restart game? (Y/N): ");
-		char restart = sc.next().charAt(0);
+	}
+	
+//	public static void send_message() { // 사회자가 플레이어들에게 메시지를 보냄
+//		
+//	}
+	
+	public static void day() { // 낮-플레이어들이 채팅&투표로 용의자 지목
 		
-		// 함수 다시 부름
-		//if(restart == 'Y') {
-		//	
-		//}
+	}
+	
+	public static void night() { // 밤-경찰/의사의 미션 & 마피아의 살인
+		
+	}
+	
+	public static void main(String[] args) {
+		while(true) {
+			waiting_room(); // 플레이어 수를 입력받고 그 수만큼 플레이어가 찰 때까지 기다림
+			
+			while(조건) { // 조건 : 마피아수=0 or 마피아수:시민수 = 1:1
+				day();
+				night();
+			}
+			
+			System.out.println("The Winner is: ");
+			System.out.println("Restart game? (Y/N): ");
+			char restart = scv.next().charAt(0);
+			if(restart == 'N')
+				break;
+		}
 	}
 
 	@Override
 	public void run() {
-		System.out.println("클라이언트 " + Thread.currentThread().getName() + " 연결됨");
+		System.out.println("Player " + Thread.currentThread().getName() + " has connected.");
 		try (
 			BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
@@ -61,15 +78,13 @@ public class Server implements Runnable {
 			};
 			
 			Stream<String> stream = Stream.generate(socketInput);
-			stream
-			.map(s -> {
+			stream.map(s -> {
 				System.out.println(Thread.currentThread().getName() + "클라이언트 요청: " + s);
 				out.println(s);
 				return s;
-			})
-			.allMatch( s -> s != null);
+			}).allMatch( s -> s != null);
 			
-			System.out.println("클라이언트 " + Thread.currentThread() + " 종료됨");
+			//System.out.println("클라이언트 " + Thread.currentThread() + " 종료됨");
 		} catch(IOException ex){
 			ex.printStackTrace();
 		}		
